ebf3352491566817fc6202c1a9323e9f6e8a323a
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-09-04 17:32:20 +0800

    feat(circos): Implement a new method for drawing circos plots
    No longer depends on the mne-connectivity package, now uses pycirclize for implementation.
    
    ---
    
    功能(circos): 实现全新的circos图绘制方法
    
    不再依赖 mne-connectivity 包，改用 pycirclize 包实现。

--- src/plotfig/__init__.py
+++ src/plotfig/__init__.py
@@ -6,14 +6,7 @@ from .bar import (
 from .correlation import plot_correlation_figure
 from .matrix import plot_matrix_figure
 from .brain_surface import plot_brain_surface_figure
-from .brain_surface_deprecated import (
-    plot_human_brain_figure,
-    plot_human_hemi_brain_figure,
-    plot_macaque_brain_figure,
-    plot_macaque_hemi_brain_figure,
-    plot_chimpanzee_brain_figure,
-    plot_chimpanzee_hemi_brain_figure,
-)
+from .circos import plot_circos_figure
 from .brain_connection import plot_brain_connection_figure, save_brain_connection_frames
 
 from importlib.metadata import version, PackageNotFoundError
@@ -22,15 +15,15 @@ __all__ = [
     "plot_one_group_bar_figure",
     "plot_one_group_violin_figure",
     "plot_multi_group_bar_figure",
+
     "plot_correlation_figure",
+
     "plot_matrix_figure",
+
     "plot_brain_surface_figure",
-    "plot_human_brain_figure",
-    "plot_human_hemi_brain_figure",
-    "plot_macaque_brain_figure",
-    "plot_macaque_hemi_brain_figure",
-    "plot_chimpanzee_brain_figure",
-    "plot_chimpanzee_hemi_brain_figure",
+
+    "plot_circos_figure",
+
     "plot_brain_connection_figure",
     "save_brain_connection_frames",
 ]
